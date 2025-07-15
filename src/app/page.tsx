1b0523503fdec685ddaae52b72b9cbcd0b86c233
Shankar Dutta <shankardutta1719@gmail.com>
2025-07-15 12:49:22 +0530

    hero and slider added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Hero from "@/components/Home/Hero";
+import Slider from "@/components/Home/Slider";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,7 +10,15 @@ export const metadata: Metadata = {
 const page = () => {
 	return (
 		<>
-			<section className="grid h-[80dvh] place-items-center"></section>
+			{/* back-ground gradient  */}
+			<div className="motion-scale-in-0 absolute top-60 left-[65%] z-[-10] h-[30%] w-[10%] rounded-full bg-gradient-to-r from-[#3dbeff] from-0% to-[#ff43fc] to-50% blur-[100px]"></div>
+			<Hero />
+			<section className="py-12">
+				<h1 className="py-4 text-center text-2xl font-bold">
+					Loved by hundreds of teams, including:
+				</h1>
+				<Slider />
+			</section>
 		</>
 	);
 };
---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { Button } from "../shadcnui/button";
+import { ArrowBigRight } from "lucide-react";
+import Image from "next/image";
+
+const Hero = () => {
+	return (
+		<>
+			<div className="grid grid-cols-1 place-content-center place-items-center gap-5 lg:h-[80dvh] lg:grid-cols-2 lg:gap-10">
+				{/* logo for mobile screen  */}
+				<Image
+					src={"/logo.svg"}
+					height={200}
+					width={200}
+					priority
+					className="block h-auto w-[250] lg:hidden"
+					alt=" logo"
+				/>
+
+				{/* text section  */}
+				<div className="motion-opacity-in-0 motion-translate-y-in-50 order-2 grid gap-5 text-center lg:order-1 lg:text-left">
+					<h1 className="text-3xl font-bold lg:text-5xl">
+						The best place to build, test, and discover front-end
+						code.
+					</h1>
+					<p className="text-lg text-black/75 dark:text-white/75">
+						CodeCraft is a social development environment for
+						front-end designers and developers. Build and deploy a
+						website, show off your work, build test cases to learn
+						and debug, and find inspiration.
+					</p>
+
+					{/* started btn  */}
+					<Link href={"/codeeditor"}>
+						<Button className="h-auto w-auto bg-blue-500 text-lg font-normal text-white hover:cursor-pointer hover:bg-blue-600">
+							Start Coding <ArrowBigRight className="pt-0.5" />
+						</Button>
+					</Link>
+				</div>
+
+				{/* hero img for all devices  */}
+				<Image
+					src={"/home/heroimg.svg"}
+					height={700}
+					width={700}
+					priority
+					className="motion-opacity-in-0 motion-translate-y-in-50 motion-preset-oscillate-sm motion-duration-5000 order-1 h-auto w-[400] lg:order-2 lg:w-auto"
+					alt="hero-side-image"
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Hero;
